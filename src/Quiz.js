642e88338a4cbf78e5b4bbaee0a605a9572ac06d
Mammen Abraham <mammenezio@gmail.com>
2022-05-23 15:44:46 +0530

    rest fix

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,6 +9,9 @@ class Quiz extends Component{
         super(props);
         this.state = {quiz_position: 1};
     }
+    handleResetClick(){
+        this.setState({quiz_position:1});
+    }
     showNextQuestion(){
         this.setState({quiz_position:this.state.quiz_position+1});
     }
@@ -16,7 +19,7 @@ class Quiz extends Component{
         const isQuizEnd = ((this.state.quiz_position -1) === quizData.quiz_questions.length)? true:false;
         return (
             <div>
-                {isQuizEnd? <QuizEnd />:
+                {isQuizEnd? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} />:
                 <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
             </div>
         )
